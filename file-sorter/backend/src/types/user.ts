98a5478d9a36fc7ee746eea99b8fa95c797006b8
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2026-03-04 20:18:48 -0500

    taller-3 user.ts user data interface

---
+++ file-sorter/backend/src/types/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    id: number;
+    firstName: string;
+    middleName: string;
+    firstLastName: string;
+    secondLastName: string;
+    phoneNumber: string;
+    address: string;
+}
