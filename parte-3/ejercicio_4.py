e4b598433f3c4269ccc2591998410859ba8314ce
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2026-03-15 16:22:12 -0500

    taller-4 - Parte 3 : ejercicio 4

---
+++ parte-3/ejercicio_4.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# Author : Cristianco9 cristian_cortes_ortiz@hotmail.com
+
+# Ejercicio 4
+"""
+Dividir el exponente en dos partes.
+"""
+# -------------------------------------------------------------------------
+# Recurrencia
+# T(b) =
+#   θ(1)          si b = 0
+#   T(b/2) + 1    si b > 0
+# -------------------------------------------------------------------------
+# análisis
+# el exponente se divide entre 2 en cada llamada
+# pasos ≈ log(b)
+# -------------------------------------------------------------------------
+
+# resultado
+# T(b) = θ(log b)
+# método ingenuo: θ(b)
+# -------------------------------------------------------------------------
+
+def potencia_divide_venceras(a, b):
+
+    if b == 0:
+        return 1
+
+    mitad = potencia_divide_venceras(a, b // 2)
+
+    if b % 2 == 0:
+        return mitad * mitad
+    else:
+        return a * mitad * mitad
+
+a = 3
+b = 13
+
+resultado = potencia_divide_venceras(a, b)
+
+print("----- POTENCIACIÓN DIVIDE Y VENCERÁS -----")
+print("Base:", a)
+print("Exponente:", b)
+print("Resultado:", resultado)
+print("-----------------------------------------")
