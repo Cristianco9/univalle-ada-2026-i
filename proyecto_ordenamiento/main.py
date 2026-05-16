bd212c4cf2d45fa63624af9d7478cd299c4f46af
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2026-05-16 11:07:45 -0500

    main script

---
+++ proyecto_ordenamiento/main.py
@@ -0,0 +1,12 @@
+from interfaz_grafica.ventana_principal import (VentanaPrincipal)
+
+
+def main():
+
+    aplicacion = (VentanaPrincipal())
+
+    aplicacion.ejecutar()
+
+
+if __name__ == "__main__":
+    main()
